2c724308f4c5c86a918a9dfcfa54f6fd03c54e15
Lukas Linss <lukas.linss@maibornwolff.de>
2024-12-02 07:54:27 +0100

    add day1 part 2

--- day1/day1part1.go
+++ day1/day1part1.go
@@ -37,6 +37,14 @@ func main() {
 	sort.Ints(sliceCol1)
 	sort.Ints(sliceCol2)
 
+	resultPart1 := part1(sliceCol1, sliceCol2)
+	fmt.Printf("result part 1 %d", resultPart1)
+
+	resultPart2 := part2(sliceCol1, sliceCol2)
+	fmt.Printf("result part 2 %d", resultPart2)
+}
+
+func part1(sliceCol1 []int, sliceCol2 []int) int {
 	var sum int
 	for i := range sliceCol1 {
 		valCol1 := sliceCol1[i]
@@ -49,9 +57,31 @@ func main() {
 			sum += valCol2 - valCol1
 		}
 	}
-	fmt.Println(sum)
+	return sum
 }
 
+func part2(col1 []int, col2 []int) int {
+	dict := make(map[int]int)
+	for _, v := range col1 {
+		occ := findOccurances(v, col2)
+		dict[v] = occ
+	}
+	sum := 0
+	for _, v := range col1 {
+		sum += v * dict[v]
+	}
+	return sum
+}
+
+func findOccurances(num int, arr []int) int {
+	occurances := 0
+	for _, v := range arr {
+		if v == num {
+			occurances++
+		}
+	}
+	return occurances
+}
 func convertToInt64(splitArray string) int64 {
 	num, _ := strconv.ParseInt(splitArray, 10, 64)
 	return num
